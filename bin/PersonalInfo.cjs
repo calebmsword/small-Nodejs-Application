3067f19c661b16351c915f08770b2d26d7c0b1b3
calebmsword <caleb.m.sword@gmail.com>
2021-06-09 14:07:10 -0400

    added address and phone number queries

--- bin/PersonalInfo.cjs
+++ bin/PersonalInfo.cjs
@@ -1,8 +1,10 @@
 //-----------------------------------------------------------------------------
 //Class used to keep track of user-entered information
 module.exports.PersonalInfo = class PersonalInfo {
-    constructor(name, ssn, momsCreditCardNumber){
+    constructor(name, address, phoneNumber, ssn, momsCreditCardNumber){
         _name: name;
+        _address: address;
+        _phoneNumber: phoneNumber;
         _ssn: ssn;
         _momsCreditCardNumber: momsCreditCardNumber;
     }
@@ -11,11 +13,15 @@ module.exports.PersonalInfo = class PersonalInfo {
     get_name(){
         return this._firstName;
     }
-
+    get_address(){
+        return this._address();
+    }
+    get_phoneNumber(){
+        return this._phoneNumber();
+    }
     get_ssn(){
         return this._ssn;
     }
-
     get_momsCreditCardNumber(){
         return this._momsCreditCardNumber;
     }
@@ -24,11 +30,15 @@ module.exports.PersonalInfo = class PersonalInfo {
     set_name(name){
         this._firstName = name;
     }
-
+    set_address(address){
+        this._address = address;
+    }
+    set_phoneNumber(phoneNumber){
+        this._phoneNumber = phoneNumber;
+    }
     set_ssn(ssn){
         this._ssn = ssn;
     }
-
     set_momsCreditCardNumber(creditCardNumber){
         this._momsCreditCardNumber = creditCardNumber;
     }
